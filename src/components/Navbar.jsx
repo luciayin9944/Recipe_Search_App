40a458780d7c20e7391d4fb8a43f1bb571ccd90a
luciayin9944 <luxiayin94@gmail.com>
2025-07-10 14:33:04 -0700

    update narbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,17 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="nav-button">Home</Link>
-            <Link to="/recipes" className="nav-button">RecipeSearch</Link>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="logo">QuickCook</div>
+        <Link to="/" className="nav-button">Home</Link>
+        <Link to="/recipes" className="nav-button">RecipeSearch</Link>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
-
 
 
